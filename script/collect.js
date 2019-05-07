d8324e96bddfa621fa0391f4f62ba63ad1f67d46
1309701836@qq.com <1309701836@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2019-05-07 11:07:27 +0000

    new13
    git-svn-id: svn://svn7.apicloud.com/A6007193486393/hbuh@46 0e1a2743-8d03-4e5a-a60c-7e26af42b145

---
+++ script/collect.js
@@ -0,0 +1,12 @@
+// <script type="text/javascript" src="../script/api.js">  </script>
+// <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+  apiready = function(){
+    // backPage();
+    // // goPage();
+    // cli();
+    // brief();
+    // collecter()
+    var name = $api.getStorage('bookName');
+    console.log(name)
+  }
